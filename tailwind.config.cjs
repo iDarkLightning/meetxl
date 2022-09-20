5e1f279fb858225add80ac2e86ec8748578ffe9e
Nirjhor Nath <nirjhor.nath@gmail.com>
2022-09-20 19:54:21 -0400

    theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: { primary: "#151515", secondary: "#1C1C1C" },
+        accent: {
+          primary: "#592483",
+          secondary: "#252525",
+          danger: "#F45050",
+          stroke: "#343232",
+        },
+      },
+    },
   },
   plugins: [],
 };
