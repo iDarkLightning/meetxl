b2586e30a3d08282e13ddc9f47a004ee1b3fe706
Nirjhor Nath <nirjhor.nath@gmail.com>
2022-09-20 19:59:09 -0400

    scaffold design system

---
+++ src/shared-components/system/button.tsx
@@ -0,0 +1,117 @@
+import React, { forwardRef, Fragment } from "react";
+import NextLink from "next/link";
+import { Url } from "url";
+import clsx from "clsx";
+import { Spinner } from "../util/spinner";
+
+const sizes = {
+  sm: "py-1 px-2 rounded-md",
+  md: "py-2 px-4 rounded-md",
+  lg: "py-3 px-6 rounded-md",
+};
+
+const variants = {
+  primary: "bg-accent-primary text-background-primary hover:bg-opacity-80",
+  secondary:
+    "border-solid border-2 border-accent-stroke bg-accent-secondary text-white hover:bg-opacity-60",
+  "outline-primary":
+    "border-solid border-2 border-accent-primary hover:bg-accent-primary hover:text-background-primary",
+  "outline-secondary":
+    "border-solid border-2 border-accent-stroke hover:bg-accent-secondary hover:text-white",
+  ghost: "",
+  danger: "bg-accent-danger text-background-primary hover:bg-opacity-80",
+  unstyled: "",
+};
+
+const variantHover = {
+  primary: "hover:bg-opacity-80",
+  secondary: "hover:bg-opacity-60",
+  "outline-primary": "hover:bg-accent-primary hover:text-background-primary",
+  "outline-secondary": "hover:bg-accent-secondary hover:text-white",
+  ghost: "hover:bg-accent-secondary",
+  danger: "hover:bg-opacity-90",
+  unstyled: "",
+};
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  /**
+   * Size of the button.
+   * @default "md"
+   */
+  size?: keyof typeof sizes;
+  /**
+   * Variant of the button.
+   * @default "secondary"
+   */
+  variant?: keyof typeof variants;
+  /**
+   * The URL to link to, via `NextLink`
+   */
+  href?: Partial<Url> | string;
+  /**
+   * Left positioned Icon
+   */
+  icon?: JSX.Element;
+  /**
+   * Whether the button is disabled
+   * @default false
+   */
+  disabled?: boolean;
+  /**
+   * Whether the button is loading
+   * @default false
+   */
+  loading?: boolean;
+}
+
+const defaultProps = {
+  size: "md",
+  variant: "secondary",
+  disabled: false,
+  loading: false,
+} as const;
+
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  (props, ref) => {
+    // spread default props, since {} is truthy
+    const { size, variant, href, icon, disabled, loading, className, ...rest } =
+      {
+        ...defaultProps,
+        ...props,
+      };
+
+    // wrap the component in a NextLink if the href is provided
+    const WrapperComponent = href ? NextLink : Fragment;
+
+    return (
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      <WrapperComponent {...(href ? { href } : ({} as any))}>
+        <button
+          ref={ref}
+          className={clsx(
+            `flex items-center truncate font-medium transition-all`,
+            sizes[size],
+            variants[variant],
+            !disabled && !loading && `${variantHover[variant]} active:scale-95`,
+            disabled && "cursor-not-allowed opacity-50",
+            className
+          )}
+          aria-disabled={disabled}
+          role={href ? "link" : "button"}
+          {...rest}
+        >
+          <span>
+            {!loading && icon}
+            {loading && <Spinner />}
+          </span>
+          <span className={clsx((icon || loading) && rest.children && "ml-2")}>
+            {props.children}
+          </span>
+        </button>
+      </WrapperComponent>
+    );
+  }
+);
+
+Button.displayName = "Button";
---
+++ src/shared-components/system/heading.tsx
@@ -0,0 +1,40 @@
+import clsx from "clsx";
+import { forwardRef } from "react";
+
+const levels = {
+  h1: "text-3xl font-medium",
+  h2: "text-2xl font-medium",
+  h3: "text-xl font-medium",
+  h4: "text-lg font-medium",
+  h5: "text-base font-medium",
+  h6: "text-sm font-medium",
+};
+
+interface HeadingProps extends React.HTMLAttributes<HTMLHeadingElement> {
+  /**
+   * The heading level.
+   * @default "h1"
+   */
+  level?: keyof typeof levels;
+}
+
+const defaultProps = {
+  level: "h2",
+} as const;
+
+export const Heading = forwardRef<HTMLHeadingElement, HeadingProps>(
+  (p, ref) => {
+    const props = { ...defaultProps, ...p };
+
+    return (
+      <props.level
+        ref={ref}
+        className={clsx(levels[props.level], props.className)}
+      >
+        {props.children}
+      </props.level>
+    );
+  }
+);
+
+Heading.displayName = "Heading";
