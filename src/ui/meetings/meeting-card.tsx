2755e10a78403732b06fa08e459ad19aecf67445
Nirjhor Nath <nirjhor.nath@gmail.com>
2022-10-21 23:35:53 -0400

    polish up meeting cards

--- src/ui/meetings/meeting-card.tsx
+++ src/ui/meetings/meeting-card.tsx
@@ -1,8 +1,13 @@
 import { Card } from "@/shared-components/system/card";
 import { Heading } from "@/shared-components/system/heading";
 import { Meeting } from "@prisma/client";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 import Link from "next/link";
 import { useOrg } from "../org/org-shell";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+
+dayjs.extend(relativeTime);
 
 export const MeetingCard: React.FC<{ meeting: Meeting }> = ({ meeting }) => {
   const org = useOrg();
@@ -10,10 +15,23 @@ export const MeetingCard: React.FC<{ meeting: Meeting }> = ({ meeting }) => {
   return (
     <Link href={`/${org.slug}/${meeting.slug}`} passHref>
       <a>
-        <Card className="bg-background-secondary py-3">
-          <Heading level="h3">{meeting.name}</Heading>
-          <div className="flex gap-4 opacity-80">
-            <p>Public: {!!meeting.isPublic ? "Yes" : "No"}</p>
+        <Card className="flex items-center justify-between bg-background-secondary py-3">
+          <div className="flex flex-col gap-2">
+            <Heading level="h3">{meeting.name}</Heading>
+            <p className="opacity-75">
+              {dayjs(meeting.startTime).isSame(dayjs(meeting.endTime), "day")
+                ? `${dayjs(meeting.startTime).format("HH:mm")} - ${dayjs(
+                    meeting.endTime
+                  ).format("HH:mm")}`
+                : `${dayjs(meeting.startTime).format("DD/MM")} - ${dayjs(
+                    meeting.endTime
+                  ).format("DD/MM")}`}
+            </p>
+          </div>
+
+          <div className="flex items-center gap-2 rounded-2xl border-[0.125rem] border-accent-stroke py-1 px-3 font-medium">
+            {meeting.isPublic ? <FaEye /> : <FaEyeSlash />}
+            <p>{meeting.isPublic ? "Public" : "Private"}</p>
           </div>
         </Card>
       </a>
