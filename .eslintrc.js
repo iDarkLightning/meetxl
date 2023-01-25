bbaf00c38da190e13bf45be9957b54e5bdedce6d
Nirjhor Nath <nirjhor.nath@gmail.com>
2023-01-25 18:57:00 -0500

    try with higher expectation of success

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
-  extends: ["prettier", "eslint:recommended"],
+  extends: ["eslint:recommended"],
   overrides: [
     {
       extends: [
