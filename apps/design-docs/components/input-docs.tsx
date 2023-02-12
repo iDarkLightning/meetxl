45aa1bd2bff56380e763499469d8a727e457edae
Nirjhor Nath <nirjhor.nath@gmail.com>
2023-02-11 19:51:46 -0500

    add disabled checkbox

--- apps/design-docs/components/input-docs.tsx
+++ apps/design-docs/components/input-docs.tsx
@@ -1,13 +1,35 @@
 import { Input } from "@meetxl/ui";
 import { Trash } from "lucide-react";
+import { useState } from "react";
 
 export const InputDocs: React.FC = () => {
+  const [disabled, setDisabled] = useState(false);
+
   return (
     <div className="flex flex-col gap-4 p-4">
+      <div className="flex gap-2">
+        <div>
+          <p>Disabled?</p>
+          <input
+            type="checkbox"
+            onChange={(e) => setDisabled(e.target.checked)}
+          />
+        </div>
+      </div>
       <p>Sizes</p>
       <div className="flex flex-col gap-2">
-        <Input size="sm" type="text" placeholder="hi there" />
-        <Input size="md" type="text" placeholder="hi there" />
+        <Input
+          size="sm"
+          type="text"
+          placeholder="hi there"
+          disabled={disabled}
+        />
+        <Input
+          size="md"
+          type="text"
+          placeholder="hi there"
+          disabled={disabled}
+        />
       </div>
       <p>Elements</p>
       <div className="flex flex-col gap-2">
@@ -15,17 +37,20 @@ export const InputDocs: React.FC = () => {
           leftElement={<Trash size="1rem" />}
           type="text"
           placeholder="hi there"
+          disabled={disabled}
         />
         <Input
           rightElement={<Trash size="1rem" />}
           type="text"
           placeholder="hi there"
+          disabled={disabled}
         />
         <Input
           leftElement={<Trash size="1rem" />}
           rightElement={<Trash size="1rem" />}
           type="text"
           placeholder="hi there"
+          disabled={disabled}
         />
       </div>
     </div>
