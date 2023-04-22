828b44cc9b750bda45fda4ad4590b856b79d70e8
Nirjhor Nath <nirjhor.nath@gmail.com>
2023-04-22 14:56:50 -0400

    position based on viewport size

--- packages/ui/src/components/toast.tsx
+++ packages/ui/src/components/toast.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Toaster as ToasterPrimitive } from "sonner";
+import useWindowSize from "../hooks/use-window-size";
 
 export const Toaster: React.FC<
   React.ComponentProps<typeof ToasterPrimitive>
 > = (props) => {
+  const { isMobile } = useWindowSize();
+
   return (
     <ToasterPrimitive
       {...props}
+      position={isMobile ? "bottom-center" : "bottom-right"}
       theme="dark"
       toastOptions={{
         className: "!bg-neutral !border-[0.025rem] !border-neutral-stroke",
