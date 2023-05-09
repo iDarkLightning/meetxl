033d8014556cad75526de8c525282f6fe08f01ae
Nirjhor Nath <nirjhor.nath@gmail.com>
2023-05-08 22:34:57 -0400

    fix build errs

--- apps/design-docs/components/accordion-docs.tsx
+++ apps/design-docs/components/accordion-docs.tsx
@@ -20,14 +20,14 @@ export const AccordionDocs: React.FC = () => {
         <AccordionItem value="item-2">
           <AccordionTrigger>Is it styled?</AccordionTrigger>
           <AccordionContent>
-            Yes. It comes with default styles that matches the other components'
+            Yes. It comes with default styles that matches the other components&apos;
             aesthetic.
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-3">
           <AccordionTrigger>Is it animated?</AccordionTrigger>
           <AccordionContent>
-            Yes. It's animated by default, but you can disable it if you prefer.
+            Yes. It&apos;s animated by default, but you can disable it if you prefer.
           </AccordionContent>
         </AccordionItem>
       </AccordionWrapper>
